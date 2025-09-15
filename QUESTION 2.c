59190e00e787d50ef18228140538eff66e1ab5ca
emmanuelahiafor21 <emmanuelahiafor21@gmail.com>
2025-09-15 09:16:44 +0000

    Update QUESTION 2.c

--- QUESTION 2.c
+++ QUESTION 2.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -6,24 +6,17 @@
 int main(int argc, char *argv[]) {
 	//Declare variable//
 	float temperature;
-	//Prompt user to input Value for temperature//
-	printf("Enter your body temperature(°C):");
+	//Prompt user to input temperature reading//
+	printf("Please enter your temperature reading in Â°C:");
 	scanf("%f",&temperature);
-	printf("Your body temperature is:%f°C\n",temperature);
 	//Using conditional statement if...else//
 	if(temperature < 35.0){
 		printf("Your temperature is low\n");
-		printf("You are not well\n");
-		printf("Medical attention needed\n");
 		
 	}else if( temperature<=37.5){
 			printf("Your body temperature is normal\n");
-			printf("No medical attention needed\n");
 		}else{
-			printf("Your temperature is high\n");
-			printf("You are not well\n");
-			printf("Medical attention needed\n");
-		
+			printf("Your temperature is high\n");	
 }
 	
 	
