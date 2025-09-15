5b33e3d855bf499a437932f6d3ab232445577022
emmanuelahiafor21 <emmanuelahiafor21@gmail.com>
2025-09-15 09:14:54 +0000

    Update QUESTION 1.c

--- QUESTION 1.c
+++ QUESTION 1.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* run this program using the console pauser or add your own getch, system("pause") or input loop *//* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	   //Declare the variables//
-float temperature;//in degree celsius//
-int heart_rate; //in bpm//
+float temperature;
+int heart_rate, blood_pressure;
 int systolic,diastolic;
-int blood_pressure;// in mmHg//
 
 //Prompt the user to input values for body temperature//
-printf("Enter your body temperature:");
+printf("Please enter your body temperature in °C:");
 scanf("%f",&temperature);
 
 //Prompt user to input value for heart rate//
-printf("Enter your heart rate:");
+printf("Please enter your heart rate in bpm:");
 scanf("%d",&heart_rate);
 
-//Prompt user to input systolic and diastolic value//
-printf("Enter your systolic:");
+//Prompt user to input systolic and diastolic values//
+printf("Please enter your blood pressure in mmHg\n");{
+printf("Systolic reading:");
 scanf("%d",&systolic);
-printf("Enter your diastolic:");
+printf("Diastolic reading:");
 scanf("%d",&diastolic);
-
+}
 //Display values with clear labels//
-printf("Vital readings\n");
-printf("Body temperature:%f �C\n",temperature);
+printf("\n --- Medical Readings ---\n");
+printf("Body temperature:%.1f °C\n",temperature);
 printf("Heart rate:%d bpm\n",heart_rate);
 printf("Blood pressure:%d/%d mmHg\n",systolic,diastolic);
 	return 0;
